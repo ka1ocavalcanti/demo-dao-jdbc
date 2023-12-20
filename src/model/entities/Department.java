baaae9cb3e06804b6a45a5fbcb2a850274730921
Kaio <kayollindoso@gmail.com>
2023-12-20 00:43:10 -0300

    Criação das class Department e Program-teste

---
+++ src/model/entities/Department.java
@@ -0,0 +1,60 @@
+package model.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Department implements Serializable{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String name;
+
+    public Department(){}
+    public Department(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Department that = (Department) o;
+
+        if (!id.equals(that.id)) return false;
+        return name.equals(that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Department { " +
+                "id=" + id +
+                ", name=" + name +
+                '}';
+    }
+}
