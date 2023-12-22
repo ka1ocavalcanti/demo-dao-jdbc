496405aa7526fbb7aeb4c1cdc53983ed86f72fe6
Kaio <kayollindoso@gmail.com>
2023-12-21 21:49:43 -0300

    Interfaces DepartmentDao e SellerDao

---
+++ src/modelDao/SellerDao.java
@@ -0,0 +1,12 @@
+package modelDao;
+
+import java.util.List;
+
+public interface SellerDao {
+
+    void insert(SellerDao obj);
+    void update(SellerDao obj);
+    void deleteById(Integer id);
+    SellerDao findById(Integer id);
+    List<SellerDao> findAll();
+}
